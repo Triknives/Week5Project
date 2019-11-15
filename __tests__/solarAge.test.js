0ad9d330d2fd064905a299de04557461f971dbf4
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-15 13:42:11 -0800

    update test structure

--- __tests__/solarAge.test.js
+++ __tests__/solarAge.test.js
@@ -1,20 +1,16 @@
 import { SolarAge } from './../src/solarYears.js';
 
-describe("user starting age", () => {
+describe("test age on each planet", () => {
   test("Should return current user age", () => {
     let userAge = new SolarAge(32);
     expect(userAge.userAge).toEqual(32);
   });
-  describe("test if user can enter 0 or less", () => {
-    test("should test 0 answers", () => {
-      let userAge = new SolarAge(0);
-      expect(userAge.getAge()).toEqual("please give a number above 0");
-    });
-    describe("test if user can enter 0 or less", () => {
-      test("should test 0 answers", () => {
-        let userAge = new SolarAge(32);
-        expect(userAge.getAge()).toEqual(8);
-      });
-    });
+  test("should test 0 answers", () => {
+    let userAge = new SolarAge(0);
+    expect(userAge.getAge()).toEqual("please give a number above 0");
+  });
+  test("should test 0 answers", () => {
+    let userAge = new SolarAge(32);
+    expect(userAge.getAge()).toEqual(8);
   });
 });
