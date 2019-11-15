01c87bbb23af3cb59d3f40b808f00e3df895293c
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-15 11:05:52 -0800

    update mercury age testing

--- src/solarYears.js
+++ src/solarYears.js
@@ -2,5 +2,9 @@
 export class SolarAge {
   constructor (userAge){
     this.userAge = userAge;
+  };
+  constructor (mercuryAge){
+    this.mercuryAge = mercuryAge;
+    mercuryAge = (userAge * 365)/88;
   }
 };
